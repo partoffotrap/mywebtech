059ce59559c2f54f75e586e299220de761b5de15
partoffotrap <a.borbotko.sas@gmail.com>
2023-06-07 00:54:51 +0500

    do 18 lesson

--- index.php
+++ index.php
@@ -1,178 +1,116 @@
 <?php
-$a = -10;
-$b = 17;
-
 echo "Первое задание:";
-echo "</br>";
-if ($a > 0 && $b > 0) {
-    echo "разность чисел: " . ($a - $b) ;
-} elseif ($a < 0 && $b < 0) {
-    echo "произведение чисел: " . ($a * $b);
-} else {
-    echo "сумма чисел: " . ($a + $b);
-}
-echo "</br>";
-?>
-
-<?php
-$a = 3;
-
-echo "Второе задание:";
-echo" </br>";
-switch ($a) {
-    case 0:
-        echo "0<br>";
-    case 1:
-        echo "1<br>";
-    case 2:
-        echo "2<br>";
-    case 3:
-        echo "3<br>";
-    case 4:
-        echo "4<br>";
-    case 5:
-        echo "5<br>";
-    case 6:
-        echo "6<br>";
-    case 7:
-        echo "7<br>";
-    case 8:
-        echo "8<br>";
-    case 9:
-        echo "9<br>";
-    case 10:
-        echo "10<br>";
-    case 11:
-        echo "11<br>";
-    case 12:
-        echo "12<br>";
-    case 13:
-        echo "13<br>";
-    case 14:
-        echo "14<br>";
-    case 15:
-        echo "15<br>";
-        break;
-    default:
-        echo "$a не в промежутке [0..15]";
-}
-?>
-
-<?php
-$a = -10;
-$b = 0;
-
-echo "Третье задание:";
-echo "</br>";
-
-function plus($a, $b) {
-    return $a + $b;
-}
-
-function minus($a, $b) {
-    return $a - $b;
-}
-
-function multiply($a, $b) {
-    return $a * $b;
-}
-
-function divide($a, $b) {
-    if ($b == 0) {
-        return "деление на ноль невозможно";
+echo "<br>";
+$number = 0;
+do {
+    if ($number == 0) {
+        echo "$number - это ноль<br>";
+    } elseif ($number % 2 == 0) {
+        echo "$number - чётное число<br>";
     } else {
-        return $a / $b;
+        echo "$number - нечётное число<br>";
     }
-}
-
-echo "сумма: " . plus($a, $b)  . "," . " разность: " . minus($a, $b)  . "," . " умноженное: " . multiply($a, $b)  . "," . " деленное: " . divide($a, $b);
-echo "</br>";
+    $number++;
+} while ($number <= 10);
 ?>
 
 <?php
-$arg1 = -10;
-$arg2 = 0;
+echo "Второе задание:";
+echo "<br>";
+$regions = [
+    "Московская область" => ["Москва", "Зеленоград", "Клин"],
+    "Ленинградская область" => ["Санкт-Петербург", "Всеволожск", "Павловск", "Кронштадт"],
+    "Рязанская область" =>["Рязань", "Касимов", "Ряжск"]];
 
+foreach ($regions as $region => $cities) {
+    echo $region . ":<br>";
+    echo implode(", ", $cities) . "<br>";
+}
+?>
+
+<?php
+echo "Третье задание:";
+echo "<br>";
+
+$translate = [
+    "а" => "a", "б" => "b", "в" => "v", "г" => "g",
+    "д" => "d", "е" => "e", "ё" => "yo", "ж" => "zh",
+    "з" => "z", "и" => "i", "й" => "y", "к" => "k",
+    "л" => "l", "м" => "m", "н" => "n", "о" => "o",
+    "п" => "p", "р" => "r", "с" => "s", "т" => "t",
+    "у" => "u", "ф" => "f", "х" => "h", "ц" => "ts",
+    "ч" => "ch", "ш" => "sh", "щ" => "sch", "ъ" => "",
+    "ы" => "y", "ь" => "'", "э" => "e", "ю" => "yu", "я" => "ya"];
+
+function translation($str) {
+    global $translate;
+    $result = '';
+    for ($i = 0; $i < mb_strlen($str); $i++) {
+        $char = mb_substr($str, $i, 1);
+        if (isset($translate[$char])) {
+            $result .= $translate[$char];
+        } else {
+            $result .= $char;
+        }
+    }
+    return $result;
+}
+echo translation('сделано третье задание');
+echo "<br>";
+?>
+
+<?php
 echo "Четвертое задание:";
-echo "</br>";
+echo "<br>";
 
-function mathOperation($arg1, $arg2, $operation) {
-    switch ($operation) {
-        case "+":
-            return plus($arg1, $arg2);
-        case "-":
-            return minus($arg1, $arg2);
-        case "*":
-            return multiply($arg1, $arg2);
-        case "/":
-            if ($arg2 == 0) {
-                return "деление на ноль невозможно";
-            } else {
-                return $arg1 / $arg2;
-            }
+$menu = [
+    "Главная",
+    "О нас",
+    "Каталог" => [
+        "Компьютеры",
+        "Телефоны",
+        "Аксессуары" => [
+            "Наушники",
+            "Чехлы",
+            "Зарядные устройства"
+        ]
+    ]
+];
+
+function doMenu($menu) {
+    echo "<ul>";
+    foreach ($menu as $key => $value) {
+        if (is_array($value)) {
+            echo "<li>$key";
+            doMenu($value);
+            echo "</li>";
+        } else {
+            echo "<li>$value</li>";
+        }
     }
+    echo "</ul>";
 }
 
-echo "сумма: " . mathOperation($arg1, $arg2, '+')  . "," . " разность: " . mathOperation($arg1, $arg2, '-')  . "," . " умноженное: " . mathOperation($arg1, $arg2, '*')  . "," . " деленное: " . mathOperation($arg1, $arg2, '/');
-echo "</br>";
+doMenu($menu);
 ?>
 
 <?php
-echo "Пятое задание:";
-echo "</br>";
-echo "Первый способ:";
-echo "</br>";
-$currentYear = date('Y');
-echo "</br>";
-?>
-
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <title> tittle </title>
-    </head>
-    <body>
-    <p> Текущий год: <?= $currentYear ?> </p>
-    </body>
-    </html>
-
-<?php
-echo "Второй способ:";
-echo "</br>";
-$currentYear = date('Y');
-require ('site.php');
-echo "</br>";
-?>
-
-<?php
-echo "Третий способ:";
-echo "</br>";
-$currentYear = date('Y');
-$content = file_get_contents('site.html');
-$content = str_replace('{{ currentYear }}', $currentYear, $content);
-echo $content;
-echo "</br>";
-?>
-
-<?php
-$val = 2;
-$pow = -2;
-
 echo "Шестое задание:";
-echo "</br>";
-function power($val, $pow) {
-    if ($val != 0 and $pow == 0) {
-        return 1;
+echo "<br>";
+
+
+$regions = [
+    "Московская область" => ["Москва", "Зеленоград", "Клин", "Коломна", "Красногорск"],
+    "Ленинградская область" => ["Санкт-Петербург", "Всеволожск", "Павловск", "Кронштадт"],
+    "Рязанская область" => ["Рязань", "Касимов", "Ряжск", "Кадом"]];
+
+foreach ($regions as $region => $cities) {
+    echo $region . ': ';
+    foreach ($cities as $city) {
+        if (mb_substr($city, 0, 1, 'UTF-8') == "К") {
+            echo $city .  " ";
+        }
     }
-    elseif ($val == 0) {
-        return 0;
-    }
-    elseif ($pow > 0) {
-        return $val * power($val, $pow - 1);
-    }
-    elseif ($pow < 0) {
-        return  1 / $val * power($val, $pow + 1);
-    }
+    echo '<br>';
 }
-echo power($val, $pow);
 ?>
--- site.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title> tittle </title>
-</head>
-<body>
-<p> Текущий год: <?= $currentYear ?> </p>
-</body>
-</html>
